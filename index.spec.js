85b0871fa7d349d8f8b81217c3f216e9beca959a
Fabien JUIF <fabien.juif@zenika.com>
2018-01-20 15:48:41 +0100

    :white_check_mark: react-redux works

--- index.spec.js
+++ index.spec.js
@@ -1,4 +1,49 @@
+/* eslint-env jest */
+/* eslint-disable
+  react/prop-types,
+  jsx-a11y/click-events-have-key-events,
+  jsx-a11y/no-static-element-interactions,
+  react/jsx-filename-extension
+*/
+import React from 'react'
+import { connect, Provider } from 'react-redux'
+import { shallow } from 'enzyme'
 import * as lib from './index'
 import tests from './misc/test-suite'
 
 tests(lib)
+
+describe('react connection', () => {
+  it('should connect redux store', () => {
+    // base component
+    const Component = ({ label, onClick }) => <div onClick={onClick}>test: {label}</div>
+
+    // connected with redux
+    const ReduxComponent = connect(
+      ({ ui: { label } }) => ({ label }),
+      dispatch => ({ onClick: dispatch({ type: 'CLICKED' }) }),
+    )(Component)
+
+    // create store with a middleware to make sure dispatch works
+    const spy = jest.fn()
+    const middlewares = [() => next => (action) => { spy(action); next(action) }]
+    const store = lib.createStore({
+      ui: { label: { type: 'simpleObject', defaultData: 'data' } },
+    }, {
+      middlewares,
+      hideRedux: false,
+    })
+
+    // react app with store provided
+    const App = () => <Provider store={store}><ReduxComponent /></Provider>
+
+    // mount app and simulate a click to test both mapStateToProps and mapDispatchToProps
+    const wrapper = shallow(<App />)
+    wrapper.simulate('click')
+
+    expect({
+      html: wrapper.html(),
+      actionDispatched: spy.mock.calls,
+    }).toMatchSnapshot()
+  })
+})
