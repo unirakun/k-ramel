5e8559b5c3ab13becec97cefddfeebe109304322
Fabien JUIF <fabien.juif@zenika.com>
2018-02-06 09:20:34 +0100

    :ok_hand: review service-worker

--- examples/todomvc/src/index.js
+++ examples/todomvc/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app.container'
-import registerServiceWorker from './registerServiceWorker'
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
