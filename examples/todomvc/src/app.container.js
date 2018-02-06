8f0e5d6749d1d4365a6fe8d2a8353a83666c6989
Fabien JUIF <fabien.juif@zenika.com>
2018-02-06 18:26:21 +0100

    :memo: todomvc

--- examples/todomvc/src/app.container.js
+++ examples/todomvc/src/app.container.js
@@ -1,11 +1,5 @@
-import { provider, inject } from 'k-simple-state/react'
-import { compose } from 'recompose'
-import loader from 'hoc-react-loader'
+import { provider } from 'k-simple-state/react'
 import store from './store'
 import App from './app'
 
-export default compose(
-  provider(store),
-  inject(st => ({ load: () => { st.dispatch({ type: '@@ui/APP_LOADED' }) } })),
-  loader(),
-)(App)
+export default provider(store)(App)
