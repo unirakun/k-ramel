3b774a2e6180d3f849ec8d1a9865eac9303bf647
Fabien JUIF <fabien.juif@zenika.com>
2018-02-07 16:45:34 +0100

    :white_check_mark:

--- misc/test-suite.js
+++ misc/test-suite.js
@@ -525,6 +525,158 @@ export default (lib) => {
         if (window) window.fetch = fetch
       })
 
+      it('should safely fail to parse json', async () => {
+        // mock
+        const mockedFetch = jest.fn(url => Promise.resolve({
+          json: () => { throw new Error(`oups-json-${url}`) },
+        }));
+        (global || window).fetch = mockedFetch
+
+        // dispatch spy
+        const spy = jest.fn()
+
+        // wait
+        let resolver
+        const wait = new Promise((resolve) => { resolver = resolve })
+
+        // store
+        const store = createStore({
+          config: { type: 'simpleObject' },
+        }, {
+          listeners: [
+            when('DISPATCHED')(async (action, st, { http }) => {
+              await http('GOOGLE')('http://google.fr', { headers: { 'Content-Type': 'application/json' } })
+              resolver()
+            }),
+            when(() => true)(action => spy(action)),
+          ],
+        })
+
+        // dispatch event
+        store.dispatch({ type: 'DISPATCHED' })
+        await wait
+
+        // assert
+        expect({
+          dispatch: spy.mock.calls,
+          fetch: mockedFetch.mock.calls,
+        }).toMatchSnapshot()
+      })
+
+
+      it('should safely fail to request', async () => {
+        // mock
+        const mockedFetch = jest.fn((url) => { throw new Error(`oups-${url}`) });
+        (global || window).fetch = mockedFetch
+
+        // dispatch spy
+        const spy = jest.fn()
+
+        // wait
+        let resolver
+        const wait = new Promise((resolve) => { resolver = resolve })
+
+        // store
+        const store = createStore({
+          config: { type: 'simpleObject' },
+        }, {
+          listeners: [
+            when('DISPATCHED')(async (action, st, { http }) => {
+              await http('GOOGLE')('http://google.fr')
+              resolver()
+            }),
+            when(() => true)(action => spy(action)),
+          ],
+        })
+
+        // dispatch event
+        store.dispatch({ type: 'DISPATCHED' })
+        await wait
+
+        // assert
+        expect({
+          dispatch: spy.mock.calls,
+          fetch: mockedFetch.mock.calls,
+        }).toMatchSnapshot()
+      })
+
+      it('should fetch data and dispatch a FAILED', async () => {
+        // mock
+        const mockedFetch = jest.fn(url => Promise.resolve({
+          status: 404,
+          url,
+        }));
+        (global || window).fetch = mockedFetch
+
+        // dispatch spy
+        const spy = jest.fn()
+
+        // wait
+        let resolver
+        const wait = new Promise((resolve) => { resolver = resolve })
+
+        // store
+        const store = createStore({
+          config: { type: 'simpleObject' },
+        }, {
+          listeners: [
+            when('DISPATCHED')(async (action, st, { http }) => {
+              await http('GOOGLE')('http://google.fr')
+              resolver()
+            }),
+            when(() => true)(action => spy(action)),
+          ],
+        })
+
+        // dispatch event
+        store.dispatch({ type: 'DISPATCHED' })
+        await wait
+
+        // assert
+        expect({
+          dispatch: spy.mock.calls,
+          fetch: mockedFetch.mock.calls,
+        }).toMatchSnapshot()
+      })
+
+      it('should fetch data and calls json()', async () => {
+        // mock
+        const mockedFetch = jest.fn(url => Promise.resolve({
+          json: () => Promise.resolve({ result: url }),
+        }));
+        (global || window).fetch = mockedFetch
+
+        // dispatch spy
+        const spy = jest.fn()
+
+        // wait
+        let resolver
+        const wait = new Promise((resolve) => { resolver = resolve })
+
+        // store
+        const store = createStore({
+          config: { type: 'simpleObject' },
+        }, {
+          listeners: [
+            when('DISPATCHED')(async (action, st, { http }) => {
+              await http('GOOGLE')('http://google.fr', { headers: { 'Content-Type': 'application/json' } })
+              resolver()
+            }),
+            when(() => true)(action => spy(action)),
+          ],
+        })
+
+        // dispatch event
+        store.dispatch({ type: 'DISPATCHED' })
+        await wait
+
+        // assert
+        expect({
+          dispatch: spy.mock.calls,
+          fetch: mockedFetch.mock.calls,
+        }).toMatchSnapshot()
+      })
+
       it('should fetch data an trigger common events', async () => {
         // mock
         const mockedFetch = jest.fn(url => Promise.resolve({
