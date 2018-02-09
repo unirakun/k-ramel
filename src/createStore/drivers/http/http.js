d0984b329f88173a071db55a0fcfc42ae85683a5
Fabien JUIF <fabien.juif@zenika.com>
2018-02-09 11:29:59 +0100

    :sparkles: add http.setAuthorization

--- src/createStore/drivers/http/http.js
+++ src/createStore/drivers/http/http.js
@@ -1,36 +1,49 @@
 const dispatchFactory = store => name => method =>
   (event, payload, status) => store.dispatch({ type: `@@http/${name}>${method}>${event}`, payload, status })
 
-export default store => name => async (url, options = {}, ...args) => {
-  // options
-  const { method = 'GET', headers = {} } = options
-  const type = headers['Content-Type'] || ''
+export default (store) => {
+  const innerHeaders = {}
 
-  // dispatcher
-  const dispatch = dispatchFactory(store)(name)(method)
+  const driver = name => async (url, options = {}, ...args) => {
+    // options
+    const { method = 'GET' } = options
+    const headers = { ...innerHeaders, ...options.headers }
+    const type = headers['Content-Type'] || ''
+    const innerOptions = { ...options, headers }
 
-  // request
-  let data
-  let raw
-  dispatch('STARTED')
-  try {
-    raw = await (global || window).fetch(url, options, ...args)
-    data = raw
+    // dispatcher
+    const dispatch = dispatchFactory(store)(name)(method)
 
-    if (type.includes('json')) {
-      data = await raw.json()
+    // request
+    let data
+    let raw
+    dispatch('STARTED')
+    try {
+      raw = await (global || window).fetch(url, innerOptions, ...args)
+      data = raw
+
+      if (type.includes('json')) {
+        data = await raw.json()
+      }
+    } catch (ex) {
+      dispatch('FAILED', ex, (raw || {}).status)
+      return ex
     }
-  } catch (ex) {
-    dispatch('FAILED', ex, (raw || {}).status)
-    return ex
+
+    const { status } = raw
+    if (status >= 400 || status < 200) {
+      dispatch('FAILED', data, status)
+    } else {
+      dispatch('ENDED', data, status)
+    }
+
+    return data
   }
 
-  const { status } = raw
-  if (status >= 400 || status < 200) {
-    dispatch('FAILED', data, status)
-  } else {
-    dispatch('ENDED', data, status)
+  // custom helpers
+  driver.setAuthorization = (authorization) => {
+    innerHeaders.Authorization = authorization
   }
 
-  return data
+  return driver
 }
