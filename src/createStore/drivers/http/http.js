59f0a53bec406876753ea3c8167ac13af4d95a19
Fabien JUIF <fabien.juif@gmail.com>
2018-02-09 17:40:34 +0100

    :sparkles: driver/http method helpers (#67)

--- src/createStore/drivers/http/http.js
+++ src/createStore/drivers/http/http.js
@@ -4,39 +4,64 @@ const dispatchFactory = store => name => method =>
 export default (store) => {
   const innerHeaders = {}
 
-  const driver = name => async (url, options = {}, ...args) => {
-    // options
-    const { method = 'GET' } = options
-    const headers = { ...innerHeaders, ...options.headers }
-    const innerOptions = { ...options, headers }
+  const driver = (name) => {
+    const ownFetch = async (url, options = {}, ...args) => {
+      // options
+      const { method = 'GET' } = options
+      const headers = { ...innerHeaders, ...options.headers }
+      const innerOptions = { ...options, headers }
 
-    // dispatcher
-    const dispatch = dispatchFactory(store)(name)(method)
+      // dispatcher
+      const dispatch = dispatchFactory(store)(name)(method)
 
-    // request
-    let data
-    let raw
-    dispatch('STARTED')
-    try {
-      raw = await (global || window).fetch(url, innerOptions, ...args)
-      data = raw
+      // request
+      let data
+      let raw
+      dispatch('STARTED')
+      try {
+        raw = await (global || window).fetch(url, innerOptions, ...args)
+        data = raw
 
-      if (raw.headers && raw.headers.get('Content-Type') && raw.headers.get('Content-Type').includes('json')) {
-        data = await raw.json()
+        if (raw.headers && raw.headers.get('Content-Type') && raw.headers.get('Content-Type').includes('json')) {
+          data = await raw.json()
+        }
+      } catch (ex) {
+        dispatch('FAILED', ex, (raw || {}).status)
+        return ex
       }
-    } catch (ex) {
-      dispatch('FAILED', ex, (raw || {}).status)
-      return ex
+
+      const { status } = raw
+      if (status >= 400 || status < 200) {
+        dispatch('FAILED', data, status)
+      } else {
+        dispatch('ENDED', data, status)
+      }
+
+      return data
     }
 
-    const { status } = raw
-    if (status >= 400 || status < 200) {
-      dispatch('FAILED', data, status)
-    } else {
-      dispatch('ENDED', data, status)
-    }
+    // methods helpers
+    ['GET', 'POST', 'HEAD', 'PUT', 'DELETE', 'OPTIONS', 'CONNECT']
+      .forEach((method) => {
+        ownFetch[method.toLowerCase()] = (url, data, options = {}) => {
+          const { headers = {} } = options
+          let innerOptions = options
 
-    return data
+          // attach data as JSON object
+          if (data && ['object', 'array'].includes(typeof data)) {
+            if (!headers['Content-Type']) innerOptions = { ...innerOptions, headers: { ...headers, 'Content-Type': 'application/json' } }
+
+            innerOptions = { ...innerOptions, body: JSON.stringify(data) }
+          }
+
+          // set method
+          innerOptions = { ...innerOptions, method }
+
+          return ownFetch(url, innerOptions)
+        }
+      })
+
+    return ownFetch
   }
 
   // custom helpers
