9429178ca4670e293295534a81903091b379cebd
Fabien JUIF <fabien.juif@zenika.com>
2018-02-16 13:19:30 +0100

    :bug: redux-devtools dispatch is catched by listeners

--- packages/k-ramel/src/createStore/enhanceRedux/enhanceRedux.js
+++ packages/k-ramel/src/createStore/enhanceRedux/enhanceRedux.js
@@ -1,21 +1,23 @@
-import { compose, applyMiddleware } from 'redux'
-import addDevTools from './addDevTools'
+import { compose } from 'redux'
+import getDevTools from './getDevTools'
 import listenFactory from './listenFactory'
 
 /* eslint-env browser */
 export default (options) => {
-  const { listeners, drivers } = options
-  let { enhancer } = options
+  const { listeners, drivers, enhancer } = options
 
-  // add redux-devtools extension (if necessary)
-  enhancer = addDevTools(options)
+  // devtools
+  const devtools = getDevTools(options)
 
   // add custom listeners extension
-  const listen = listenFactory(listeners, drivers)
+  const listen = listenFactory(listeners, drivers, !!devtools)
+
+  const enhancers = [
+    enhancer,
+    devtools,
+    listen.enhancer,
+  ].filter(Boolean)
 
   // add this middleware to enhancer
-  const middleware = applyMiddleware(listen.middleware)
-  if (enhancer) return { enhancer: compose(middleware, enhancer), listen }
-
-  return { enhancer: middleware, listen }
+  return { enhancer: compose(...enhancers), listen }
 }
