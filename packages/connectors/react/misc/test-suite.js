75dedf9f026b1a2bf7f363594543de6a02120e9d
Valentin Cocaud <v.cocaud@gmail.com>
2018-02-16 17:01:37 +0100

    :white_check_mark: tests for empty inject params and empty inject function return

--- packages/connectors/react/misc/test-suite.js
+++ packages/connectors/react/misc/test-suite.js
@@ -151,6 +151,34 @@ export default (lib) => {
       expect(wrapper.find('div').html()).toMatchSnapshot()
     })
 
+    it('should not throw when using inject without params', () => {
+      // store
+      const testStore = createStore({
+        config: { type: 'simpleObject' },
+      })
+
+      // tested component
+      const Component = () => <div />
+      const WrappedComponent = inject()(Component)
+
+      // test
+      mount(<WrappedComponent />, { context: { store: testStore } })
+    })
+
+    it('should not throw when inject function return undefined', () => {
+      // store
+      const testStore = createStore({
+        config: { type: 'simpleObject' },
+      })
+
+      // tested component
+      const Component = () => <div />
+      const WrappedComponent = inject(() => {})(Component)
+
+      // test
+      mount(<WrappedComponent />, { context: { store: testStore } })
+    })
+
     it('should inject callbacks', () => {
       // store
       const testStore = createStore({
