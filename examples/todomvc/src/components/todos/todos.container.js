36c0826bc6282e84a2fd6c5bafb38cc36101c7ce
Fabien JUIF <fabien.juif@gmail.com>
2018-04-05 15:31:26 +0200

    :sparkles: listen with new context API

--- examples/todomvc/src/components/todos/todos.container.js
+++ examples/todomvc/src/components/todos/todos.container.js
@@ -1,8 +1,13 @@
-import { inject } from '@k-ramel/react'
+import { compose } from 'recompose'
+import { inject, listen } from '@k-ramel/react'
 import Component from './todos'
+import listeners from './todos.listeners'
 
-export default inject(store => ({
-  todos: store.ui.views[store.ui.footer.get().filter].get(),
-  allCompleted: store.ui.footer.get().todosLeft === 0,
-  onCompleteAll: () => store.dispatch('@@ui/HEADER_ON_COMPLETE_ALL'),
-}))(Component)
+export default compose(
+  inject(store => ({
+    todos: store.ui.views[store.ui.footer.get().filter].get(),
+    allCompleted: store.ui.footer.get().todosLeft === 0,
+    onCompleteAll: () => store.dispatch('@@ui/HEADER_ON_COMPLETE_ALL'),
+  })),
+  listen(listeners, 'NAME'),
+)(Component)
--- packages/connectors/react/src/listen.jsx
+++ packages/connectors/react/src/listen.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React from 'react'
+import createContext from './createContext'
 import getWrappedDisplayName from './getWrappedDisplayName'
 
 const defaultListeners = []
@@ -8,32 +9,58 @@ const toActionFactory = (name) => {
   return type => `@@krml/LISTENERS>${type}${suffix}`
 }
 
-export default (listeners = defaultListeners, name) => (WrappedComponent) => {
+const defaultObject = {}
+
+const wrapper = (listeners, name) => (WrappedComponent) => {
   const toAction = toActionFactory(name)
 
-  return class extends Component {
-    static displayName = `listen(${getWrappedDisplayName(WrappedComponent)}`
-
-    static contextTypes = {
-      store: () => null, // this is to avoid importing prop-types
+  return class extends React.Component {
+    static propTypes = {
+      store: () => null,
+      ownProps: () => null,
     }
 
-    componentWillMount() {
-      const { store } = this.context
+    static defaultProps = {
+      store: undefined,
+      ownProps: defaultObject,
+    }
+
+    componentDidMount() {
+      const { store } = this.props
 
       store.listeners.add(listeners)
       store.dispatch(toAction('ADDED'))
     }
 
     componentWillUnmount() {
-      const { store } = this.context
+      const { store } = this.props
 
       store.dispatch(toAction('REMOVING'))
       store.listeners.remove(listeners)
     }
 
     render() {
-      return <WrappedComponent {...this.props} />
+      return <WrappedComponent {...this.props.ownProps} />
     }
+  }
+}
+
+export default (listeners = defaultListeners, name) => {
+  const { Consumer } = createContext()
+
+  const withListeners = wrapper(listeners, name)
+
+  return (Component) => {
+    const WrappedComponent = withListeners(Component)
+
+    const WithConsumer = props => (
+      <Consumer>
+        {store => <WrappedComponent ownProps={props} store={store} />}
+      </Consumer>
+    )
+
+    WithConsumer.displayName = `listen(${getWrappedDisplayName(Component)}`
+
+    return WithConsumer
   }
 }
