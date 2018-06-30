eb9b0b596a0fea9bdb9363594bf37b67ef7558e3
Fabien JUIF <fabien.juif@gmail.com>
2018-06-30 15:10:30 +0200

    :bug: window undefined on node

--- packages/k-ramel/src/createStore/enhanceRedux/getDevTools.js
+++ packages/k-ramel/src/createStore/enhanceRedux/getDevTools.js
@@ -1,10 +1,14 @@
-const getReduxDevToolsEnhancer = name => window.devToolsExtension({ name })
+const getReduxDevToolsEnhancer = (name) => {
+  if (typeof window === 'undefined') return undefined
+  if (window.devToolsExtension) return window.devToolsExtension({ name })
+  return undefined
+}
 
 export default (options) => {
   const { name, devtools } = options
 
   // no devtool enable
-  if (!devtools || !window || !window.devToolsExtension) return undefined
+  if (!devtools) return undefined
 
   // return enhancer with devtools
   return getReduxDevToolsEnhancer(name)
