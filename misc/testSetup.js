c0b54a189364bcd1b040a386bc3f01501fe56c17
Fabien JUIF <fabien.juif@gmail.com>
2019-02-05 21:04:52 +0100

    :arrow_up: upgrade --latest (all)

--- misc/testSetup.js
+++ misc/testSetup.js
@@ -1,4 +1,6 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
+global.regeneratorRuntime = require('regenerator-runtime')
+
 Enzyme.configure({ adapter: new Adapter() })
