31ba8b38c80e0f27c0802661b5ca5375013bf5e9
Fabien JUIF <fabien.juif@gmail.com>
2019-02-26 11:47:14 +0100

    oupsy

--- packages/connectors/react/misc/test-suite.js
+++ packages/connectors/react/misc/test-suite.js
@@ -233,7 +233,7 @@ export default (lib) => {
       expect(wrapper.find('div').html()).toMatchSnapshot()
     })
 
-    it.only('should refresh when props changes', () => {
+    it('should refresh when props changes', () => {
       // store
       const testStore = createStore({
         config: { type: 'simple.object' },
