5adba7f7a1d2f90f998812ec41611da09bb12400
Arthur FAUGERAS <arthur.faugeras@gmail.com>
2019-10-30 10:32:18 +0100

    :ok_hand: use declare module

--- packages/connectors/react/typing/index.d.ts
+++ packages/connectors/react/typing/index.d.ts
@@ -1,19 +1,11 @@
-import React from "react";
-import { StoreBase, when } from "k-ramel";
-export declare const provider = <P extends StoreBase>(store: P) => (
-  component: React.ReactComponentElement
-) => React.ReactComponentElement;
+declare module '@k-ramel/react' {
+  import React from 'react'
+  import { StoreBase, when } from 'k-ramel'
+  export const provider = <P extends StoreBase>(store: P) => (component: React.ReactComponentElement) => React.ReactComponentElement
 
-export declare type StoreToProps<Store = {}, Props = {}, Drivers = {}> = (
-  store: Store,
-  props: Props,
-  drivers: Drivers
-) => any;
+  export type StoreToProps<Store = {}, Props = {}, Drivers = {}> = (store: Store, props: Props, drivers: Drivers) => any
 
-export declare const inject = <Store = {}, Props = {}, Drivers = {}>(
-  storeToProps: StoreToProps<Store, Props, Drivers>
-) => (component: React.ReactComponentElement) => React.ReactComponentElement;
+  export const inject = <Store = {}, Props = {}, Drivers = {}>(storeToProps: StoreToProps<Store, Props, Drivers>) => (component: React.ReactComponentElement) => React.ReactComponentElement
 
-export declare const listen = (listeners: Array, name?: string) => (
-  component: React.ReactComponentElement
-) => React.ReactComponentElement;
+  export const listen = (listeners: Array, name?: string) => (component: React.ReactComponentElement) => React.ReactComponentElement
+}
--- packages/drivers/http/typing/index.d.ts
+++ packages/drivers/http/typing/index.d.ts
@@ -1,32 +1,30 @@
-import { StoreBase, Driver, BaseAction } from "k-ramel";
+declare module '@k-ramel/driver-http' {
+  import { StoreBase, Driver, BaseAction } from 'k-ramel'
 
-declare const httpDriver: () => Driver;
+  const httpDriver: () => Driver
 
-export declare type HTTPOptions = Omit<body, RequestInit>;
-export declare type RequestFunction = (
-  url: string,
-  body?: BodyInit | object,
-  options?: HTTPOptions
-) => void;
+  export type HTTPOptions = Omit<body, RequestInit>
+  export type RequestFunction = (url: string, body?: BodyInit | object, options?: HTTPOptions) => void
 
-export declare type HTTPDriver = (
-  name: string,
-  context?: object
-) => {
-  post: RequestFunction;
-  get: RequestFunction;
-  head: RequestFunction;
-  patch: RequestFunction;
-  put: RequestFunction;
-  delete: RequestFunction;
-  options: RequestFunction;
-  connect: RequestFunction;
-};
+  export type HTTPDriver = (
+    name: string,
+    context?: object,
+  ) => {
+    post: RequestFunction
+    get: RequestFunction
+    head: RequestFunction
+    patch: RequestFunction
+    put: RequestFunction
+    delete: RequestFunction
+    options: RequestFunction
+    connect: RequestFunction
+  }
 
-export declare interface HTTPBaseAction<Payload> extends BaseAction {
-  payload: Payload;
-  status: number;
-  headers: Headers;
+  export interface HTTPBaseAction<Payload> extends BaseAction {
+    payload: Payload
+    status: number
+    headers: Headers
+  }
+
+  export default httpDriver
 }
-
-export default httpDriver;
