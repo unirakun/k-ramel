e9772ea2f71ddb6ced651a59df54623ba11fb871
AFaugeras <arthur.faugeras@gmail.com>
2019-11-13 16:44:23 +0100

    :bug: ts / fix function type in k-ramel

--- packages/k-ramel/dist/index.d.ts
+++ packages/k-ramel/dist/index.d.ts
@@ -1,54 +1,65 @@
-declare module 'k-ramel' {
-  import { TypeDefinition, TypeDefinitionFunction } from 'k-redux-factory'
-  import { compose, applyMiddleware, Dispatch, enhanceMiddleware } from 'redux'
-  export { Types } from 'k-redux-factory'
+declare module "k-ramel" {
+  import {
+    TypeDefinition,
+    TypeDefinitionFunction,
+    ReducerType
+  } from "k-redux-factory";
+  import { compose, applyMiddleware, Dispatch, enhanceMiddleware } from "redux";
+  export { Types } from "k-redux-factory";
   export type StoreOptions = {
-    hideRedux?: boolean
-    enhancer?: any
-    init?: any
-    listeners?: any
-    devtools?: any
-    trace?: boolean
-    traceLimit?: number
-    name?: string
-    drivers?: any
-  }
+    hideRedux?: boolean;
+    enhancer?: any;
+    init?: any;
+    listeners?: any;
+    devtools?: any;
+    trace?: boolean;
+    traceLimit?: number;
+    name?: string;
+    drivers?: any;
+  };
 
   export interface StoreDefinition {
-    [key: string]: StoreDefinition | TypeDefinition
+    [key: string]: StoreDefinition | TypeDefinition;
   }
 
   export interface StoreDefinitionBase {
-    [key: string]: StoreDefinitionBase | ReducerType
+    [key: string]: StoreDefinitionBase | ReducerType;
   }
 
   export type API = {
-    [key: string]: any
-  }
+    [key: string]: any;
+  };
   export type Driver<Store extends StoreDefinitionBase> = {
-    getDriver: (store: Store) => API
+    getDriver: (store: Store) => API;
     getReducer?: () => {
-      path: string
-      reducers: StoreDefinitionBase
-    }
-    init?: (store: Store) => void
-    getEnhancer?: () => ReturnValue<typeof compose> | ReturnValue<typeof enhanceMiddleware>
-  }
+      path: string;
+      reducers: StoreDefinitionBase;
+    };
+    init?: (store: Store) => void;
+    getEnhancer?: () =>
+      | ReturnValue<typeof compose>
+      | ReturnValue<typeof enhanceMiddleware>;
+  };
 
-  export function createStore<Store extends StoreDefinitionBase>(definition: StoreDefinition, options?: StoreOptions): Store
-  export const types: TypeDefinitionFunction
+  export function createStore<Store extends StoreDefinitionBase>(
+    definition: StoreDefinition,
+    options?: StoreOptions
+  ): Store;
+  export const types: TypeDefinitionFunction;
 
   export interface StoreBase extends StoreDefinitionBase {
-    dispatch: Dispatch
+    dispatch: Dispatch;
   }
 
   export type BaseAction = {
-    type: string
-  }
+    type: string;
+  };
 
-  export interface ReactionType<Action extends BaseAction = BaseAction, Store = {}, Drivers = {}> extends Function {
-    (action: Action, store: Store, drivers: Drivers): void
-  }
+  export type ReactionType<
+    Action extends BaseAction = BaseAction,
+    Store = {},
+    Drivers = {}
+  > = (action: Action, store: Store, drivers: Drivers) => void;
 
-  export const when = (type: string | RegExp) => (reaction: typeof ReactionType) => {}
+  export const when = (type: string | RegExp) => (reaction: any) => {};
 }
