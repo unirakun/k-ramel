3a261e74049624dbfb3bf173810257462008c8af
Fabien JUIF <fabien.juif@gmail.com>
2019-11-13 13:51:11 +0100

    :arrow_up: eslint

--- packages/connectors/react/src/inject.jsx
+++ packages/connectors/react/src/inject.jsx
@@ -14,102 +14,105 @@ const withoutFunctions = object => Object
     defaultObject,
   )
 
-export default injectFunction => WrappedComponent => class extends Component {
-  static displayName = `inject(${getWrappedDisplayName(WrappedComponent)})`
+export default injectFunction => (WrappedComponent) => {
+  class InjectedComponent extends Component {
+    constructor(props, context) {
+      super(props, context)
 
-  static contextTypes = {
+      this.first = true
+      this.reduxState = undefined
+      this.state = {
+        injectedProps: {},
+      }
+    }
+
+    UNSAFE_componentWillMount() { // eslint-disable-line camelcase
+      const { store } = this.context
+
+      if (!store) {
+        const bold = 'font-weight: bolder; font-style: italic;'
+        // eslint-disable-next-line no-console
+        console.error(
+          `[k-ramel/react] Error in %cinject%c for the component %c${getWrappedDisplayName(WrappedComponent)}%c\n` +
+          '\t> The store needs to be provided by an ancestor of this component.\n' +
+          '\t> You can use %cprovider%c from %c@k-ramel/react%c or %cProvider%c from %creact-redux%c.\n\n' +
+          'Check the documentation for an example at https://github.com/alakarteio/k-ramel#connect-it-with-reactjs\n',
+          bold, '', bold, '', bold, '', bold, '', bold, '', bold, '',
+        )
+        return
+      }
+      this.store = store
+
+      // run in once
+      this.inject()
+
+      // subscribe
+      this.unsubscribe = store.subscribe(() => {
+        // the state doesn't change, so we don't have to call containers
+        if (this.store && this.reduxState === this.store.getState()) return
+
+        this.inject()
+      })
+    }
+
+    UNSAFE_componentWillReceiveProps(nextProps) { // eslint-disable-line camelcase
+      if (shallowEqual(this.props, nextProps)) return
+
+      this.inject(nextProps)
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+      if (this.first) return true
+
+      const { injectedProps } = this.state // eslint-disable-line react/prop-types
+
+      return !(
+        shallowEqual(this.props, nextProps)
+        && shallowEqual(
+          withoutFunctions(nextState.injectedProps),
+          withoutFunctions(injectedProps),
+        )
+      )
+    }
+
+    componentWillUnmount() {
+      this.store = undefined
+      this.unsubscribe()
+    }
+
+    inject = (nextProps) => {
+      if (!this.store) return
+
+      this.setState(state => ({
+        ...state,
+        injectedProps: injectFunction
+          ? injectFunction(this.store, nextProps || this.props, this.store.drivers) || defaultObject
+          : defaultObject,
+      }))
+
+      this.reduxState = this.store.getState()
+    }
+
+    render() {
+      if (this.first) this.first = false
+
+      const { injectedProps } = this.state
+
+      return (
+        <WrappedComponent
+          /* this is parent props */
+          {...this.props}
+          /* this is injected props from hoc */
+          {...injectedProps}
+        />
+      )
+    }
+  }
+
+  InjectedComponent.displayName = `inject(${getWrappedDisplayName(WrappedComponent)})`
+  InjectedComponent.contextTypes = {
     store: () => null, // this is to avoid importing prop-types
   }
 
-  constructor(props, context) {
-    super(props, context)
-
-    this.first = true
-    this.reduxState = undefined
-    this.state = {
-      injectedProps: {},
-    }
-  }
-
-  componentWillMount() {
-    const { store } = this.context
-
-    if (!store) {
-      const bold = 'font-weight: bolder; font-style: italic;'
-      // eslint-disable-next-line no-console
-      console.error(
-        `[k-ramel/react] Error in %cinject%c for the component %c${getWrappedDisplayName(WrappedComponent)}%c\n` +
-        '\t> The store needs to be provided by an ancestor of this component.\n' +
-        '\t> You can use %cprovider%c from %c@k-ramel/react%c or %cProvider%c from %creact-redux%c.\n\n' +
-        'Check the documentation for an example at https://github.com/alakarteio/k-ramel#connect-it-with-reactjs\n',
-        bold, '', bold, '', bold, '', bold, '', bold, '', bold, '',
-      )
-      return
-    }
-    this.store = store
-
-    // run in once
-    this.inject()
-
-    // subscribe
-    this.unsubscribe = store.subscribe(() => {
-      // the state doesn't change, so we don't have to call containers
-      if (this.store && this.reduxState === this.store.getState()) return
-
-      this.inject()
-    })
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (shallowEqual(this.props, nextProps)) return
-
-    this.inject(nextProps)
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    if (this.first) return true
-
-    const { injectedProps } = this.state // eslint-disable-line react/prop-types
-
-    return !(
-      shallowEqual(this.props, nextProps)
-      && shallowEqual(
-        withoutFunctions(nextState.injectedProps),
-        withoutFunctions(injectedProps),
-      )
-    )
-  }
-
-  componentWillUnmount() {
-    this.store = undefined
-    this.unsubscribe()
-  }
-
-  inject = (nextProps) => {
-    if (!this.store) return
-
-    this.setState(state => ({
-      ...state,
-      injectedProps: injectFunction
-        ? injectFunction(this.store, nextProps || this.props, this.store.drivers) || defaultObject
-        : defaultObject,
-    }))
-
-    this.reduxState = this.store.getState()
-  }
-
-  render() {
-    if (this.first) this.first = false
-
-    const { injectedProps } = this.state
-
-    return (
-      <WrappedComponent
-        /* this is parent props */
-        {...this.props}
-        /* this is injected props from hoc */
-        {...injectedProps}
-      />
-    )
-  }
+  return InjectedComponent
 }
